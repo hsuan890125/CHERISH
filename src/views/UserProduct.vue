c8d49e48c8bc68225a76ea3287aac52d5029633f
bbyoulike5487 <bbyoulike5487@gmail.com>
2023-12-20 21:11:03 +0800

    修改：UserProduct 樣式

--- src/views/UserProduct.vue
+++ src/views/UserProduct.vue
@@ -2,25 +2,54 @@
     <Loading :active="isLoading"></Loading>
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb">
-        <li class="breadcrumb-item"><router-link to="/nav/allProducts">{{ product.content }}</router-link></li>
+        <li class="breadcrumb-item"><router-link to="/nav/allProducts" class="link-secondary text-decoration-none border-bottom border-secondary">所有商品</router-link></li>
         <li class="breadcrumb-item active" aria-current="page">{{ product.title }}</li>
       </ol>
     </nav>
-    <div class="row justify-content-center">
+    <div class="row row-cols-1 row-cols-lg-2 g-3 justify-content-center">
       <article class="col">
-        <img :src="product.imageUrl" alt="" class="img-fluid mb-3">
+        <img :src="product.imageUrl" alt="productImage" class="img-fluid mb-3">
       </article>
       <div class="col">
-        <h2>{{ product.title }}</h2>
-        <div class="h5" v-if="product.price === product.origin_price">NT$ {{ product.origin_price }}</div>
-        <del class="h6" v-if="product.price !== product.origin_price">NT$ {{ product.origin_price }}</del>
-        <div class="h5" v-if="product.price !== product.origin_price">NT$ {{ product.price }}</div>
+        <div class="d-flex justify-content-between">
+          <div class="d-flex">
+            <h2 class="ls mb-0">{{ product.title }}</h2>
+            <p class="text-danger ms-2" style="font-size: 10px;">SALE</p>
+          </div>
+          <div class="d-flex align-items-end">
+            <div class="h4 mb-0" v-if="product.price === product.origin_price">NT$ {{ product.origin_price }}</div>
+            <del class="h5 text-muted me-2 mb-0" v-if="product.price !== product.origin_price">NT$ {{ product.origin_price }}</del>
+            <div class="h4 mb-0" v-if="product.price !== product.origin_price">NT$ {{ product.price }}</div>
+          </div>
+        </div>
         <hr>
-        <div>{{ product.description }}</div>
+        <div class="text-primary ls mb-5">{{ product.description }}</div>
+        <div class="d-flex justify-content-between">
+          <form class="d-flex flex-column text-primary border-bottom border-primary">
+            <label for="productNum" class="form-label fs-4 ls">數量</label>
+            <div class="input-group">
+              <button class="btn btn-outline-primary fs-5 border-0 productNumBtn" type="button"
+                @click.prevent="refreshQty(-1)"
+                :disabled="productQty === 1">–
+              </button>
+              <input type="number" id="productNum" class="form-control text-center" aria-label="productNum" v-model="productQty">
+              <button class="btn btn-outline-primary fs-5 border-0 productNumBtn" type="button"
+                @click.prevent="refreshQty(1)">+
+              </button>
+            </div>
+          </form>
+          <div class="d-flex align-items-end">
+            <button type="button" class="text-primary bg-warning fs-3 border-0 mx-1"><i class="bi bi-suit-heart"></i></button>
+          </div>
+        </div>
         <hr>
-        <button type="button" class="btn btn-outline-danger"
-                @click="addToCart(product.id)">
-          加到購物車
+        <button type="button" class="btn btn-outline-primary w-100"
+          @click="addToCart(product.id)">
+          加入購物車
+        </button>
+        <button type="button" class="btn btn-outline-primary w-100 mt-3"
+              >
+          立即購買
         </button>
       </div>
     </div>
@@ -32,6 +61,7 @@ export default {
         return {
             product: {},
             id: '',
+            productQty: 1,
             };
     },
     methods: {
@@ -45,6 +75,9 @@ export default {
                 this.product = response.data.product;
                 }
             });
+        },
+        refreshQty(number) {
+          this.productQty += (number);
         },
         addToCart(id, qty = 1) {
             const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart`;
