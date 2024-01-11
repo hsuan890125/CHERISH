f913906b7437228953c07a04cb0126bffc233348
bbyoulike5487 <bbyoulike5487@gmail.com>
2024-01-11 16:51:38 +0800

    製作：scrollToTop

--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -3,6 +3,11 @@
     <ToastMessages></ToastMessages>
     <router-view/>
     <Footer></Footer>
+    <div class="position-fixed end-0 bottom-0 pe-3 pe-lg-5" v-if="isShow" style="z-index: 100;">
+        <button type="button" class="btn p-3" @click="scrollToTop" style="background-color: #B1AF98;">
+            <i class="bi bi-arrow-up fs-3 text-light"></i>
+        </button>
+    </div>
 </template>
 
 <script>
@@ -12,6 +17,11 @@ import ToastMessages from '@/components/ToastMessages.vue';
 import Footer from '@/components/UserFooter.vue';
 
 export default {
+    data() {
+        return {
+            isShow: false
+        }
+    },
     components: {
         UserNav,
         ToastMessages,
@@ -22,7 +32,28 @@ export default {
             emitter,
         };
     },
-    created() {
+    methods: {
+        scrollToTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+        },
+        isShowScroll() {
+            const rootElement = document.documentElement;
+            const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+            if ((rootElement.scrollTop / scrollTotal) > 0.10) {
+                this.isShow = true;
+            } else {
+                this.isShow = false;
+            }
+        }
     },
+    mounted() {
+        document.addEventListener('scroll', this.isShowScroll)
+    },
+    unmounted() {
+        document.removeEventListener('scroll', this.isShowScroll)
+    }
 }
 </script>
