7d856b6db737fddc8374f6b653b1190cc1d8b26f
bbyoulike5487 <bbyoulike5487@gmail.com>
2024-01-11 16:02:48 +0800

    製作：footer

--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -2,17 +2,20 @@
     <UserNav></UserNav>
     <ToastMessages></ToastMessages>
     <router-view/>
+    <Footer></Footer>
 </template>
 
 <script>
 import UserNav from '@/components/UserNav.vue';
 import emitter from '@/methods/emitter';
 import ToastMessages from '@/components/ToastMessages.vue';
+import Footer from '@/components/UserFooter.vue';
 
 export default {
     components: {
         UserNav,
-        ToastMessages
+        ToastMessages,
+        Footer
     },
     provide() {
         return {
