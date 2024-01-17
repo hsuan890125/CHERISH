f69477f0ae6299c08a180e9b401f9de17b774d89
bbyoulike5487 <bbyoulike5487@gmail.com>
2024-01-17 16:31:49 +0800

    部屬

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()],
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/CHERISH/'
+    : '/'
 })
