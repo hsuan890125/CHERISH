69de5da185c4e5dded2202c31161883f1fb5407e
bbyoulike5487 <bbyoulike5487@gmail.com>
2024-02-01 18:21:31 +0800

    修改：isLoading 位置

--- src/views/UserAllProducts.vue
+++ src/views/UserAllProducts.vue
@@ -1,83 +1,82 @@
 <!-- eslint-disable vuejs-accessibility/click-events-have-key-events -->
 <template>
-    <Loading :active="isLoading"/>
-    <div class="container mb-5 mb-lg-6">
-      <div class="row row-cols-1 row-cols-lg-2 mt-6 mt-lg-7">
-        <div class="col-lg-2">
-          <ul class="mb-5 pd-menu">
-            <li class="list-unstyled my-3 position-relative">
-              <a href="#"
-                class="link-primary text-decoration-none"
-                @click.prevent="categoryItem = ''">
-                <span class="list-hover stretched-link"
-                  :class="{ active: categoryItem === ''}"
-                >— </span>ALL
-              </a>
-            </li>
-            <li class="list-unstyled my-3 position-relative">
-              <a href="#"
-                class="link-primary text-decoration-none"
-                @click.prevent="categoryItem = '戒指'">
-                <span class="list-hover stretched-link"
-                  :class="{ active: categoryItem === '戒指'}"
-                >— </span>RING
-              </a>
-            </li>
-            <li class="list-unstyled my-3 position-relative">
-              <a href="#"
-                class="link-primary text-decoration-none"
-                @click.prevent="categoryItem = '耳環'">
-                <span class="list-hover stretched-link"
-                  :class="{ active: categoryItem === '耳環'}"
-                >— </span>EARRING
-              </a>
-            </li>
-            <li class="list-unstyled my-3 position-relative">
-              <a href="#"
-                class="link-primary text-decoration-none"
-                @click.prevent="categoryItem = '手鍊'">
-                <span class="list-hover stretched-link"
-                  :class="{ active: categoryItem === '手鍊'}"
-                >— </span>BRACELET
-              </a>
-            </li>
-            <li class="list-unstyled my-3 position-relative">
-              <a href="#"
-                class="link-primary text-decoration-none"
-                @click.prevent="categoryItem = '項鍊'">
-                <span class="list-hover stretched-link"
-                  :class="{ active: categoryItem === '項鍊'}"
-                >— </span>NECKLACE
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-lg-10">
-          <div class="row row-cols-2 row-cols-lg-5 mt-lg-5">
-            <div class="col px-2 mb-3"
-              v-for="item in productsFilter[pagination.current_page-1]"
-              :key="item.id">
-              <div class="card border-0 h-100 cardHover" @click="getProduct(item.id)">
-                <div class="imageHover">
-                  <img :src="item.imageUrl" class="card-img-top img-fluid" :alt="item.title">
-                </div>
-                <div
-                  class="card-body
-                    text-primary
-                    bg-warning
-                    px-0
-                    d-flex
-                    flex-column
-                    justify-content-between"
-                >
-                  <p class="mb-0 ls">{{ item.title }}</p>
-                  <div v-if="item.price === item.origin_price">NT$ {{ item.origin_price }}</div>
-                  <div v-if="item.price !== item.origin_price">
-                    <del class="small text-muted">NT$ {{ item.origin_price }}</del>
-                    <div>
-                      NT$ {{ item.price }}
-                        <span class="text-danger ms-1" style="font-size: 10px;">SALE</span>
-                    </div>
+  <Loading :active="isLoading"/>
+  <div class="container mb-5 mb-lg-6">
+    <div class="row row-cols-1 row-cols-lg-2 mt-6 mt-lg-7">
+      <div class="col-lg-2">
+        <ul class="mb-5 pd-menu">
+          <li class="list-unstyled my-3 position-relative">
+            <a href="#"
+              class="link-primary text-decoration-none"
+              @click.prevent="categoryItem = ''">
+              <span class="list-hover stretched-link"
+                :class="{ active: categoryItem === ''}"
+              >— </span>ALL
+            </a>
+          </li>
+          <li class="list-unstyled my-3 position-relative">
+            <a href="#"
+              class="link-primary text-decoration-none"
+              @click.prevent="categoryItem = '戒指'">
+              <span class="list-hover stretched-link"
+                :class="{ active: categoryItem === '戒指'}"
+              >— </span>RING
+            </a>
+          </li>
+          <li class="list-unstyled my-3 position-relative">
+            <a href="#"
+              class="link-primary text-decoration-none"
+              @click.prevent="categoryItem = '耳環'">
+              <span class="list-hover stretched-link"
+                :class="{ active: categoryItem === '耳環'}"
+              >— </span>EARRING
+            </a>
+          </li>
+          <li class="list-unstyled my-3 position-relative">
+            <a href="#"
+              class="link-primary text-decoration-none"
+              @click.prevent="categoryItem = '手鍊'">
+              <span class="list-hover stretched-link"
+                :class="{ active: categoryItem === '手鍊'}"
+              >— </span>BRACELET
+            </a>
+          </li>
+          <li class="list-unstyled my-3 position-relative">
+            <a href="#"
+              class="link-primary text-decoration-none"
+              @click.prevent="categoryItem = '項鍊'">
+              <span class="list-hover stretched-link"
+                :class="{ active: categoryItem === '項鍊'}"
+              >— </span>NECKLACE
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-10">
+        <div class="row row-cols-2 row-cols-lg-5 mt-lg-5">
+          <div class="col px-2 mb-3"
+            v-for="item in productsFilter[pagination.current_page-1]"
+            :key="item.id">
+            <div class="card border-0 h-100 cardHover" @click="getProduct(item.id)">
+              <div class="imageHover">
+                <img :src="item.imageUrl" class="card-img-top img-fluid" :alt="item.title">
+              </div>
+              <div
+                class="card-body
+                  text-primary
+                  bg-warning
+                  px-0
+                  d-flex
+                  flex-column
+                  justify-content-between"
+              >
+                <p class="mb-0 ls">{{ item.title }}</p>
+                <div v-if="item.price === item.origin_price">NT$ {{ item.origin_price }}</div>
+                <div v-if="item.price !== item.origin_price">
+                  <del class="small text-muted">NT$ {{ item.origin_price }}</del>
+                  <div>
+                    NT$ {{ item.price }}
+                      <span class="text-danger ms-1" style="font-size: 10px;">SALE</span>
                   </div>
                 </div>
               </div>
@@ -85,8 +84,9 @@
           </div>
         </div>
       </div>
-      <Pagination :pages="pagination" @emitPages="getProducts"/>
     </div>
+    <Pagination :pages="pagination" @emitPages="getProducts"/>
+  </div>
 </template>
 
 <script>
@@ -108,8 +108,8 @@ export default {
   },
   methods: {
     getProducts(page = 1) { // 取得各頁商品列表
-      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products/?page=${page}`;
       this.isLoading = true;
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products/?page=${page}`;
       this.$http.get(api)
         .then((res) => {
           this.products = res.data.products;
