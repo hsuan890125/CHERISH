76a97569fba3cd93aed8a2f95904b4dc5b19715c
bbyoulike5487 <bbyoulike5487@gmail.com>
2024-02-07 14:53:12 +0800

    修改：管理員登入按鈕位置

--- src/components/UserFooter.vue
+++ src/components/UserFooter.vue
@@ -22,20 +22,13 @@
           <a href="#" class="fs-3"><i class="bi bi-facebook"></i></a>
         </div>
       </div>
-      <!-- CMS -->
-      <div class="row row-cols-1 row-cols-lg-2 pb-5 pb-lg-6">
-        <div class="col-lg-5">
-          <h5>CMS</h5>
-        </div>
-        <div class="col-lg-7 mt-4 mt-lg-0">
-          <router-link to="/dashboard/login" class="text-decoration-none link-hover">
-            管理員登入 <i class="bi bi-arrow-right"></i>
-          </router-link>
-        </div>
-      </div>
     </div>
     <p class="text-muted text-center ls mb-0 pb-4">
-      個人作品無商業用途<br>
+      個人作品無商業用途｜
+      <router-link to="/dashboard/login" class="text-decoration-none link-hover">
+        管理員登入 <i class="bi bi-arrow-right"></i>
+      </router-link>
+      <br>
       Copyright © 2024 Meng-Hsuan Wu. All rights reserved.
     </p>
   </footer>
